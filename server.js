ca43b77ea4497166160ed99da44036d38af63c95
maucou <maucou123@hotmail.com>
2023-02-18 12:47:08 -0300

    Database connection created

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('Conectei a base')
+        app.emit('pronto')
+    }).catch(e => console.log('e'))
+
 const routes = require('./routes')
 const path = require('path')
-const {middlewareGlobal, outroMiddleware} = require('./src/middlewares/middleware')
+const {middlewareGlobal} = require('./src/middlewares/middleware')
 
 
 app.use(express.urlencoded({extended: true}))
@@ -11,14 +20,17 @@ app.use(express.static(path.resolve(__dirname, 'public')))
 app.set('views', path.resolve(__dirname, 'src', 'views'))
 app.set('view engine', 'ejs')
 
-//Middlewares
+//Middleware
 app.use(middlewareGlobal)
-app.use(outroMiddleware)
 app.use(routes)
 
-app.listen(3000, () =>{
-    console.log('Acesse http://localhost:3000/')
+app.on('pronto', () => {
+    app.listen(3000, () =>{
+        console.log('Acesse http://localhost:3000/')
+    })
 })
+
+
 
 
 
